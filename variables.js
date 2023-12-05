cb205c3053c23466030817ede0badd0a4e9ecc01
nhatmobile1 <nhatmobile@gmail.com>
2023-12-05 08:55:33 -0800

    work on js fundamentals practice

--- variables.js
+++ variables.js
@@ -1,4 +1,28 @@
-let a = prompt("First number?", 1);
-let b = prompt("Second number?", 2);
+// let a = prompt("First number?", 1);
+// let b = prompt("Second number?", 2);
 
-alert(+a + +b);
+// alert(+a + +b);
+// console.log(23 + 97);
+
+// console.log(1 + 2 + 4 + 5 + 6 + 10 + 200);
+
+// let mathEx = (4 + 6 + 9) / 77;
+
+// console.log(mathEx);
+//
+
+// const maxNum = 57;
+
+// let actual = maxNum - 13;
+
+// let percentage = actual / maxNum;
+
+// Three ways to decalre variable: var, let, const
+
+// let name = "nhat";
+
+// alert(`hello ${1}`);
+
+// alert(`hello ${"name"}`);
+
+// alert(`hello ${name}`);
